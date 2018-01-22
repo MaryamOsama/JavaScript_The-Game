7c6205a2be893468776fc9e4d2e9f693847b3d66
Mohab <34834902+mohab2017@users.noreply.github.com>
2018-01-22 11:20:37 +0200

    Editing Ball Object script file

--- js/Objects/ball.js
+++ js/Objects/ball.js
@@ -1,20 +1,18 @@
 
 var Ball=function(ballx,bally,radius) {
-    this.ballX=ballx;
-    this.ballY=bally;
-    this.ballRadius=radius;
-    this.ballShape=new Image(this.ballX,this.ballY);
-    this.ballShape.src="img/ball.png";
+    var ballX, ballY, ballRadius;
     
 }
 Ball.prototype.drawBall=function(){
     
     ctx.beginPath();
-    ctx.drawImage(this.ballShape,this.ballX,this.ballY,40,40)
-
-    /* ctx.arc(this.ballx,this.bally,this.ballRadius,0,Math.PI*2)
-    ctx.fillStyle="red";
-    ctx.fill(); */
+    ctx.arc(this.ballX, this.ballY, this.ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#23207d";
+    ctx.fill();
     ctx.closePath();
 }
-
+Ball.prototype.init=function(ballX,ballY,ballRadius){
+    this.ballX=ballX;
+    this.ballY=ballY;
+    this.ballRadius=ballRadius;
+};
