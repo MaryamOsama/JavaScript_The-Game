5fed0bfd55179ffb2ca113be2216d09f716c13c1
MaryamOsama <maryam.badreldin@gmail.com>
2018-01-22 22:40:20 +0200

    merged

--- js/Objects/ball.js
+++ js/Objects/ball.js
@@ -1,18 +1,19 @@
-
-var Ball=function(ballx,bally,radius) {
+function Ball(){
     var ballX, ballY, ballRadius;
-    
+    //var ballStyle=ballStyle;
 }
 Ball.prototype.drawBall=function(){
-    
     ctx.beginPath();
     ctx.arc(this.ballX, this.ballY, this.ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#23207d";
     ctx.fill();
     ctx.closePath();
-}
+};
 Ball.prototype.init=function(ballX,ballY,ballRadius){
     this.ballX=ballX;
     this.ballY=ballY;
     this.ballRadius=ballRadius;
 };
+
+
+
